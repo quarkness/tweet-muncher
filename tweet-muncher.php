e2d41a1b2d437f0269913f2f3dee0993eeafc1ec
Ivo van Doesburg <ivo@nrcdigitaal.nl>
2011-03-13 12:56:05 +0100

    query twitter

--- tweet-muncher.php
+++ tweet-muncher.php
@@ -1,28 +1,28 @@
 <?php
 /**
  * Plugin Name: Tweet Muncher
- * Plugin URI: http://labs.nrcmedia.nl/
+ * Plugin URI: http://voorgevorderden.com/
  * Description: Imports certain tweets as posts
  * Version: 0.1
  * Author: Ivo van Doesburg
- * Author URI: http://labs.nrcmedia.nl/
+ * Author URI: http://voorgevorderden.com/
  *
  */
 
-
-function twm_load_defaults() {
-	return Array(
+function twm_activate() {
+	wp_get_current_user();
+	update_option('twm_settings', Array(
 		'category' => 'Tweet',
 		'search' => 'belachelijk',
+		'author_id' => $current_user->ID,
+		)
 	);
-}
-
-function twm_activate() {
-	update_option('twm_settings', twm_load_defaults());
+	update_option('twm_since_id', 0);
 }
 
 function twm_deactivate() {
 	delete_option('twm_settings');
+	delete_option('twm_since_id');
 }
 
 function twm_admin_page()
@@ -52,13 +52,58 @@ function twm_admin_page()
 	echo '</div>';
 }
 
+function twm_test()
+{
+	$settings = get_option('twm_settings');
+	$since_id = get_option('twm_since_id');
+	$base = 'http://search.twitter.com/search.json?';
+	$url = $base . http_build_query(Array('q'=>$settings['search'], 'since_id'=>$since_id));
+	$response = twm_get_json_api($url);
+	echo "<pre>";
+	print_r($response);
+	if($response['status'] != 'error')
+	{
+//		update_option('twm_since_id', $response['results'][0]['id']);
+		foreach($response['results'] as $tweet)
+		{
+			$post_author = $settings['author_id'];
+			$post_date = strtotime($tweet['created_at']);
+//			$post_date = $post_date + 3600; // Uurtje extra, er gaat iets mis met de tijdzone ...
+			$post_date = date('Y-m-d H:i:s', $post_date);
+			
+			$postdata = compact('post_author', 'post_date', 'post_content', 'post_title', 'post_status');
+			$cat = get_term_by('name', $settings['category'], 'category');
+			$postdata['post_category'] = array($cat->term_id);
+			$post_id = wp_insert_post($postdata);
+			add_post_meta( $post_id, 'twm_tweet', $tweet, true );
+		}
+	}
+}
 
 add_action('admin_menu', 'twm_add_options_page');
 function twm_add_options_page()
 {
-	add_options_page( 'Tweet Muncher', 'Tweet Muncher', 8, 'tweet-muncher.php', 'twm_admin_page' );
+	add_options_page( 'Tweet Muncher', 'Tweet Muncher', 8, 'tweet-muncher-admin', 'twm_admin_page' );
+	add_options_page( 'Tweet Muncher Test', 'Tweet Muncher Test', 8, 'tweet-muncher-test', 'twm_test' );
 }
 
+function twm_get_json_api($url)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$response = curl_exec($ch);
+	$info = curl_getinfo($ch);
+	if(strlen(curl_error($ch) > 0))
+		return Array('status'=>'error', 'error'=>curl_error($ch));
+
+	// als response te json_decoden is, gedecodeerd teruggeven, anders status error
+	if($d = json_decode($response, true))
+		return $d;
+	else
+		return Array('status'=>'error', 'error'=>'http status ' . $info['http_code']);
+}
+	
 register_activation_hook(__FILE__, 'twm_activate');
 register_deactivation_hook( __FILE__, 'twm_deactivate');
 
